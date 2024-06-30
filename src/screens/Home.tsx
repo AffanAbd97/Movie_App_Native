0a50f35a2303674e60919c0ed907cff2c5369f5a
Affan-97 <affana959@gmail.com>
2024-06-30 10:26:53 +0700

    fix: eslintBug

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
 
-import type { MovieListProps } from '../../types/app'
-import MovieList from '../components/movies/MovieList'
 import { HomeStackNavigation } from '../navigations/StackNavigation'
 
 function Home(): JSX.Element {
